4e9e1d25a540b115cbb9afaaa4e8e55b6e225479
Birger Retterstøl Olaisen <birger.retterstol.olaisen@gmail.com>
2022-08-25 19:44:02 +0200

    Added code for setting booster settings

--- examples.py
+++ examples.py
@@ -37,3 +37,32 @@ def make_amp_types():
                gain=15,volume=25,bass=35,middle=45,treble=55,presence=65)
     f.append(p5)
     f.save()
+
+def make_bst_types():
+    from waza_tsl import TSLFile, Patch, Color, BST_type
+    f = TSLFile("File with bst types")
+    p1 = Patch("TREBLE BOOST")
+    p1.set_bst(BST_type.TREBLE_BOOST,Color.GREEN,drive=10,bottom=-50,tone=-45,
+               solo_sw=0,solo_level=14,effect_level=16,direct_mix=18)
+    f.append(p1)
+    p2 = Patch("FAT DS")
+    p2.set_bst(BST_type.FAT_DS,Color.RED,drive=30,bottom=-30,tone=-25,
+               solo_sw=1,solo_level=34,effect_level=36,direct_mix=38)
+    f.append(p2)
+    p3 = Patch("BLUES DRIVE")
+    p3.set_bst(BST_type.BLUES_DRIVE,Color.YELLOW,drive=50,bottom=-10,tone=-5,
+               solo_sw=0,solo_level=54,effect_level=56,direct_mix=58)
+    f.append(p3)
+    p4 = Patch("T-SCREAM")
+    p4.set_bst(BST_type.T_SCREAM,Color.GREEN,drive=80,bottom=+10,tone=+5,
+               solo_sw=1,solo_level=74,effect_level=76,direct_mix=78)
+    f.append(p4)
+    p5 = Patch("DST+")
+    p5.set_bst(BST_type.DSTPLUS,Color.RED,drive=100,bottom=+30,tone=+25,
+               solo_sw=0,solo_level=84,effect_level=86,direct_mix=88)
+    f.append(p5)
+    p6 = Patch("'60s FUZZ")
+    p6.set_bst(BST_type.SIXTIES_FUZZ,Color.YELLOW,drive=120,bottom=+50,tone=+45,
+               solo_sw=1,solo_level=94,effect_level=96,direct_mix=98)
+    f.append(p6)
+    f.save()
--- waza_tsl.py
+++ waza_tsl.py
@@ -44,6 +44,33 @@ class AMP_type(Enum):
     BROWN    = 23
     LEAD     = 24
 
+class Color(Enum):
+    GREEN  = 0
+    RED    = 1
+    YELLOW = 2
+
+class BST_type(Enum):
+    MID_BOOST    =  0
+    CLEAN_BOOST  =  1
+    TREBLE_BOOST =  2
+    CRUNCH_OD    =  3
+    NATURAL_OD   =  4
+    WARM_OD      =  5
+    FAT_DS       =  6
+    METAL_DS     =  8
+    OCT_FUZZ     =  9
+    BLUES_DRIVE  = 10
+    OVERDRIVE    = 11
+    T_SCREAM     = 12 # T-SCREAM
+    TURBO_OD     = 13
+    DISTORTION   = 14
+    RAT          = 15
+    GUV_DS       = 16
+    DSTPLUS      = 17 # DST+
+    METAL_ZONE   = 18
+    SIXTIES_FUZZ = 19 # '60s FUZZ
+    MUFF_FUZZ    = 20
+
 
 class Patch:
     def __init__(self,name="New patch"):
@@ -61,7 +88,7 @@ class Patch:
         return self.data["paramSet"]["User%Patch"][index]
     def __setitem__(self,index,value):
         if value is None:
-            value = CLEAR_DATA[index]
+            value = CLEAR_DATA[index] # if value is None, use default value
         if index in KNOWN_INDEXES:
             name,case,limits = KNOWN_INDEXES[index]
         else:
@@ -91,8 +118,24 @@ class Patch:
         self[87] = presence
         self[88] = volume
 
-    def set_bst(self,bst_type,**kw):
-        raise NotImplementedError
+    def set_bst(self,bst_type,color,drive=None,bottom=None,tone=None,
+                solo_sw=None,solo_level=None,effect_level=None,direct_mix=None):
+        if isinstance(bst_type, BST_type):
+            self[49] = bst_type.value
+        else:
+            raise TypeError(f"bst_type must be an BST_type, not '{type(bst_type)}'")
+        self[48] = 1 # BST on
+        self[50] = drive
+        self[51] = bottom
+        self[52] = tone
+        self[53] = solo_sw
+        self[54] = solo_level
+        self[55] = effect_level
+        self[56] = direct_mix
+        index = 2305 + color.value
+        self[index] = bst_type.value
+        self[2320] = color.value
+        self[2325] = 0 # BST, not MOD
 
     def set_mod(self,mod_type,**kw):
         raise NotImplementedError
@@ -119,4 +162,19 @@ KNOWN_INDEXES = {
     86 : ["AMP:TREBLE",   "minmax", [0,100]],
     87 : ["AMP:PRESENCE", "minmax", [0,100]],
     88 : ["AMP:VOLUME",   "minmax", [0,100]],
+    # BST:
+    2305 : ["BST:GREEN TYPE",   "listed", [0,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20]],
+    2306 : ["BST:RED TYPE",     "listed", [0,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20]],
+    2307 : ["BST:YELLOW TYPE",  "listed", [0,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20]],
+    2320 : ["BST:COLOR",        "listed", [0,1,2]],
+    2325 : ["BST OR MOD",       "listed", [0,1]],
+    48   : ["BST:ON/OFF",       "listed", [0,1]],
+    49   : ["BST:TYPE",         "listed", [0,1,2,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20]],
+    50   : ["BST:DRIVE",        "minmax", [0,120]],
+    51   : ["BST:BOTTOM",       "scaled", [50,1,0,100]],
+    52   : ["BST:TONE",         "scaled", [50,1,0,100]],
+    53   : ["BST:SOLO SW",      "listed", [0,1]],
+    54   : ["BST:SOLO LEVEL",   "minmax", [0,100]],
+    55   : ["BST:EFFECT LEVEL", "minmax", [0,100]],
+    56   : ["BST:DIRECT MIX",   "minmax", [0,100]],
 }
